c42e0667570266052e90a07c434940896dfeb13a
Matern, Tobias <tobias.matern@lht.dlh.de>
2019-09-08 12:44:32 +0200

    Initial Svelte App with child components

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+    import NavBar from './NavBar.svelte'
+    import Home from './pages/Home.svelte';
+
+	let other = 'x';
+</script>
+
+<style>
+    :global(body) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+	.container {
+	    max-width: 950px;
+	}
+</style>
+
+<div class="container">
+    <NavBar></NavBar>
+    <Home></Home>
+</div>
+
---
+++ src/NavBar.svelte
@@ -0,0 +1,19 @@
+<style>
+	.nav-bar {
+	    display: flex;
+	}
+
+	.nav-element {
+	    margin-left: 5px;
+	    margin-right: 5px;
+	}
+</style>
+
+<div class="nav-bar">
+    <div class="nav-element">
+        Home
+    </div>
+    <div class="nav-element">
+        Über Mich
+    </div>
+</div>

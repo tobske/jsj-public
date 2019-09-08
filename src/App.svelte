2747122aaa7a6f20eed25a8541772643f0b943e0
Matern, Tobias <tobias.matern@lht.dlh.de>
2019-09-08 13:22:48 +0200

    feat: basic background and man-background

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,11 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+
+        background-color: #e7b556;
+        background-image: url("/background-man.jpg"), url("/background.jpg");
+        background-position: center 315px, center top;
+        background-repeat: no-repeat;
     }
 
 	.container {

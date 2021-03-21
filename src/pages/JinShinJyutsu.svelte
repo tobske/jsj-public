2de1f776e93fef7b06a59f9660a40cb2d73c20ee
jennylein18 <67802540+jennylein18@users.noreply.github.com>
2021-03-21 20:20:32 +0100

    Updated Text on Jin Shin Jyutsu

--- src/pages/JinShinJyutsu.svelte
+++ src/pages/JinShinJyutsu.svelte
@@ -5,7 +5,10 @@
 <div in:fade>
     <h1>Jin Shin Jyutsu</h1>
     <p>
-        Jin Shin Jyutsu® ist eine alte, von Jiro Murai wieder entdeckte Kunst aus Japan. Die Behandlung bietet eine wirkungsvolle Hilfe, die von einfachen Variationen auch vom Behandelten selbst erlernt und weitergeführt werden kann. Diese Methode hat den Vorteil, dass sie jederzeit zur Verfügung steht und fast überall einsetzbar ist: in der U-Bahn, bei Konferenzen, beim Warten auf den Bus - die ideale Heilweise für den modernen Menschen.
+        Jin Shin Jyutsu® ist eine alte, von Jiro Murai wieder entdeckte Kunst aus Japan. Die Behandlung bietet eine wirkungsvolle Hilfe, die von einfachen Variationen auch vom Behandelten selbst erlernt und weitergeführt werden kann. Diese Methode hat den Vorteil, dass sie jederzeit zur Verfügung steht und fast überall einsetzbar ist: in der U-Bahn, bei Konferenzen, beim Warten auf den Bus - die ideale Heilweise für den modernen Menschen.<br>
         Beim Jin Shin Jyutsu® wird mit sechsunszwanzig "Sicherheitsenergieschlössern" gearbeitet. Ist eines oder mehrere dieser Schlösser blockiert, kann die sich daraus ergebende Blockade unsere Lebensenergie schwächen. Diese Schlösser sitzen an unterschiedlichen Stellen unseres Körpers. In dem die Fingerspitzen oder Handflächen in verschiedenen Kombinationen aufgelegt werden, hamonisieren sich Körper, Geist und Seele. Schmerzzustände sowie chronische und akute Krankheiten werden gelindert.
+    </p>
+    <p>
+        Jin shin Jyutsu® ist die Kunst des Glücklichseins, die Kunst der Langlebigkeit und die Kunst der Güte.
     </p>
 </div>

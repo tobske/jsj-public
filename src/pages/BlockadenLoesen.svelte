b94f7649c41826640f385e75eac548823d24ec96
jennylein18 <67802540+jennylein18@users.noreply.github.com>
2021-03-21 20:24:18 +0100

    Updated Text on Blockaden Loesen

--- src/pages/BlockadenLoesen.svelte
+++ src/pages/BlockadenLoesen.svelte
@@ -6,28 +6,20 @@
 <div in:fade>
 	<h1>Stellvertretend Blockaden lösen</h1>
 	<p>
-		Menschen-Systeme geraten gelegentlich aus dem Gleichgewicht. Insbesondere Familiensysteme.
-		Dies kann sich bei einem selbst in Lebens- und gesundheitlichen Themen, bei denen man alleine einfach nicht mehr weiterkommt, äußern.
+		Menschen und Systeme geraten gelegentlich aus dem Gleichgewicht, insbesondere Familiensysteme. <br>
+		Dies kann sich in offenen Lebensfragen und gesundheitlichen Blockaden äußern. Traumatische Erfahrungen, die von Betroffenen nicht verarbeitet wurden, bleiben häufig nicht nur für diese selbst eine starke Belastung, sondern auch in nachfolgenden Generationen können sich diese ungelösten Erlebnisse z.B. in Träumen, unerklärlichen Ängsten oder Zwängen, Schuldgefühlen, mangelndem Selbstwert, gesundheitlichen Problemen…zeigen. Wo die Aufarbeitung nicht oder nur unvollständig gelingt, kann dies zur Last für Urenkel/innen und weitere Generationen werden.	</p>
+	<p>
+		Silke Heinisch und Anne Steinmann (bzw. Ines Haftmann oder Vera Frohberg) bilden in einem geschützten Rahmen Ihr Anliegen ab.<br>
+		Sie zeigen die dahinter liegenden Zusammenhänge auf, lösen – soweit möglich- Verstrickungen und finden eine neue heilsame Ordnung. So kann Versöhnung stattfinden und sich die Möglichkeit für Veränderung bieten.
+
 	</p>
 	<p>
-		Traumatische Erfahrungen, die von Betroffenen nicht verarbeitet und integriert werden können, bleiben nicht nur für diese selbst eine lebenslange Belastung.
-		Es können transgenerationale Übertragungsphänomene in den nachfolgenden Generationen auftreten und sich bei den Nachkommen z.B. in Träumen, Phantasien, im Selbstbild,
-		durch unerklärliche Ängste oder Zwänge, Gefühle von Schuld, (Selbst-) Fremdheit oder Zwangshandlungen zeigen.
-		Wo die Aufarbeitung nicht oder nur unvollständig gelingt, kann die Gefühlserbschaft zur Last auch noch für Urenkel*innen und weitere Generationen werden.
+		Auch unerkannte Gaben und Potentiale von unseren Ahnen können so entdeckt, gestärkt und gelebt werden.<br>
+		Im beruflichen Kontext können auf diese Weise Unternehmens-Systeme bereinigt werden und zur Entstehung einer positiven Unternehmenskultur beitragen. Dies führt zu einem Arbeitsleben mit noch mehr Verständnis, Wertschätzung und gesteigerter Produktivität.
+
 	</p>
 	<p>
-		Es kann aber auch um ein „gutes“ Erbe gehen, um die Gaben, die wir ebenfalls durch unsere Ahnen erhalten haben und darum, diese einzigartige Kraft des Lebens zu wecken,
-		anzunehmen und zu entfalten. Vielleicht leben wir dieses Potential nicht, weil wir es nicht zu erkennen vermögen, was sich ebenfalls in z.B. gesundheitlichen
-		oder anderen Lebens-Themen äußern kann.
-	</p>
-	<p>
-		Silke Heinisch, Jin Shin Jyutsu® Praktikerin mit schamanisch-systemischem Ansatz, und Anne Steinmann bilden für Sie und mit Ihnen in einem geschützten Rahmen
-		Ihr Anliegen ab und machen Blockaden sichtbar - vergleichbar einer systemischen Aufstellung. Sie richten den Blick auf das Ganze und die dahinter liegenden Zusammenhänge,
-		lösen - soweit möglich - Verstrickungen auf und finden eine neue, heilsame Ordnung. So kann Versöhnung stattfinden und sich durch die ganzheitliche Erfahrung eines neuen,
-		heilsamen Bildes die Möglichkeit zu Veränderung bieten.
-	</p>
-	<p>
-		Auch im beruflichen Umfeld können auf diese Weise Hemmnisse bereinigt werden und ein effizientes, freudiges Miteinander mit Kolleg*innen und Vorgesetzten in Teams,
-		Abteilungen und Unternehmen entstehen lassen. Für ein Arbeitsleben mit Verständnis, Wertschätzung, Gelassenheit und letztendlich größerer Produktivität.
+		4 Hände und 2 Herzen<br>
+		Alternativ oder als Nachbehandlung können Sie sich mit Jin Shin Jyutsu behandeln lassen.
 	</p>
 </div>

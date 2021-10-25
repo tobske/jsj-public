9c966ce3502b2a9ee14b116114bd2e8011929e95
jennylein18 <67802540+jennylein18@users.noreply.github.com>
2021-10-25 22:20:12 +0200

    Changed text

--- src/routes/blockaden-loesen.svelte
+++ src/routes/blockaden-loesen.svelte
@@ -4,12 +4,12 @@
 
 
 <div in:fade>
-	<h1>Stellvertretend Blockaden lösen</h1>
+	<h1>Blockaden lösen</h1>
 	<p>
 		Menschen und Systeme geraten gelegentlich aus dem Gleichgewicht, insbesondere Familiensysteme. <br>
 		Dies kann sich in offenen Lebensfragen und gesundheitlichen Blockaden äußern. Traumatische Erfahrungen, die von Betroffenen nicht verarbeitet wurden, bleiben häufig nicht nur für diese selbst eine starke Belastung, sondern auch in nachfolgenden Generationen können sich diese ungelösten Erlebnisse z.B. in Träumen, unerklärlichen Ängsten oder Zwängen, Schuldgefühlen, mangelndem Selbstwert, gesundheitlichen Problemen…zeigen. Wo die Aufarbeitung nicht oder nur unvollständig gelingt, kann dies zur Last für Urenkel/innen und weitere Generationen werden.	</p>
 	<p>
-		Silke Heinisch und Anne Steinmann (bzw. Ines Haftmann oder Vera Frohberg) bilden in einem geschützten Rahmen Ihr Anliegen ab.<br>
+		Silke Heinisch und Anne Steinmann oder Vera Frohberg bilden in einem geschützten Rahmen Ihr Anliegen ab.<br>
 		Sie zeigen die dahinter liegenden Zusammenhänge auf, lösen – soweit möglich- Verstrickungen und finden eine neue heilsame Ordnung. So kann Versöhnung stattfinden und sich die Möglichkeit für Veränderung bieten.
 
 	</p>
@@ -20,6 +20,6 @@
 	</p>
 	<p>
 		4 Hände und 2 Herzen<br>
-		Alternativ oder als Nachbehandlung können Sie sich mit Jin Shin Jyutsu behandeln lassen.
+		Alternativ oder als Nachbehandlung können Sie sich mit Jin Shin Jyutsu oder Therapeutic Touch behandeln lassen.
 	</p>
 </div>

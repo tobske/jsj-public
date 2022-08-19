79dccfbcca3eb8bab79e55f6c13408392597e545
tobske <tobske@posteo.de>
2022-08-19 06:51:12 +0000

    Deploying to master from @ tobske/jsj-public@8004e63270b09b118d9fba907940124307a993df 🚀

--- docs/_app/pages/index.svelte-5f181b75.js
+++ docs/_app/pages/index.svelte-5f181b75.js
@@ -1,9 +1,9 @@
-import{S as x,i as E,s as A,k as w,e as l,t as b,O as B,d as s,m as z,c as h,a as c,h as S,g as k,H as t,I as H,P as J,Q as W,R as P}from"../chunks/vendor-85234c82.js";function V(L){let i,e,a,u,m,d,f,g,o,v,_;return{c(){i=w(),e=l("div"),a=l("h1"),u=b("Herzlich Willkommen"),m=w(),d=l("p"),f=b(`Jeder Mensch ist einzigartig und so wie er ist vollkommen. Wir erhalten st\xE4ndig die n\xF6tigen
+import{S as x,i as E,s as A,k as b,e as l,t as w,O as B,d as s,m as z,c as h,a as c,h as S,g as k,H as t,I as H,P as J,Q as W,R as P}from"../chunks/vendor-8bf3bb9e.js";function V(L){let i,e,a,u,m,d,f,g,o,v,_;return{c(){i=b(),e=l("div"),a=l("h1"),u=w("Herzlich Willkommen"),m=b(),d=l("p"),f=w(`Jeder Mensch ist einzigartig und so wie er ist vollkommen. Wir erhalten st\xE4ndig die n\xF6tigen
         Informationen, um als \u201ESystem Mensch\u201C vital zu bleiben. H\xE4ufig verstehen wir diese
         Botschaften nicht mehr und geraten aus unserer Mitte. Auf der Suche nach einer L\xF6sung
         \xFCbersehen wir, dass wir selbst f\xFCr uns verantwortlich sind. In der Folge reagieren wir
         entweder gar nicht oder unangemessen. Jede Ver\xE4nderung im System tr\xE4gt die Information der
-        L\xF6sung schon in sich.`),g=w(),o=l("p"),v=b(`Meine Gabe und damit Aufgabe ist es, die in diesem System innewohnenden Botschaften, also
+        L\xF6sung schon in sich.`),g=b(),o=l("p"),v=w(`Meine Gabe und damit Aufgabe ist es, die in diesem System innewohnenden Botschaften, also
         L\xF6sungsans\xE4tze zu sehen und in eine f\xFCr den Klienten verst\xE4ndliche Sprache zu \xFCbersetzen.`),this.h()},l(n){B('[data-svelte="svelte-1f7jgic"]',document.head).forEach(s),i=z(n),e=h(n,"DIV",{});var r=c(e);a=h(r,"H1",{});var y=c(a);u=S(y,"Herzlich Willkommen"),y.forEach(s),m=z(r),d=h(r,"P",{});var I=c(d);f=S(I,`Jeder Mensch ist einzigartig und so wie er ist vollkommen. Wir erhalten st\xE4ndig die n\xF6tigen
         Informationen, um als \u201ESystem Mensch\u201C vital zu bleiben. H\xE4ufig verstehen wir diese
         Botschaften nicht mehr und geraten aus unserer Mitte. Auf der Suche nach einer L\xF6sung

8ad3fe49692b9e238f3422b515f74eb9f31709eb
tobske <tobske@posteo.de>
2022-09-02 11:11:08 +0000

    Deploying to master from @ tobske/jsj-public@7d2ccd8472b1cf7bc69b61dcbd71430db22acb10 🚀

---
+++ docs/_app/pages/index.svelte-6a13a2e2.js
@@ -0,0 +1,13 @@
+import{S as x,i as E,s as A,k as w,e as l,t as b,N as B,d as s,m as z,c as h,a as c,h as S,g as I,H as t,D as k,O as J,P as W}from"../chunks/index-91aa14bd.js";import{f as P}from"../chunks/index-6c2fb6dc.js";function V(L){let i,e,a,u,m,d,f,g,o,v,_;return{c(){i=w(),e=l("div"),a=l("h1"),u=b("Herzlich Willkommen"),m=w(),d=l("p"),f=b(`Jeder Mensch ist einzigartig und so wie er ist vollkommen. Wir erhalten st\xE4ndig die n\xF6tigen
+        Informationen, um als \u201ESystem Mensch\u201C vital zu bleiben. H\xE4ufig verstehen wir diese
+        Botschaften nicht mehr und geraten aus unserer Mitte. Auf der Suche nach einer L\xF6sung
+        \xFCbersehen wir, dass wir selbst f\xFCr uns verantwortlich sind. In der Folge reagieren wir
+        entweder gar nicht oder unangemessen. Jede Ver\xE4nderung im System tr\xE4gt die Information der
+        L\xF6sung schon in sich.`),g=w(),o=l("p"),v=b(`Meine Gabe und damit Aufgabe ist es, die in diesem System innewohnenden Botschaften, also
+        L\xF6sungsans\xE4tze zu sehen und in eine f\xFCr den Klienten verst\xE4ndliche Sprache zu \xFCbersetzen.`),this.h()},l(n){B('[data-svelte="svelte-1f7jgic"]',document.head).forEach(s),i=z(n),e=h(n,"DIV",{});var r=c(e);a=h(r,"H1",{});var y=c(a);u=S(y,"Herzlich Willkommen"),y.forEach(s),m=z(r),d=h(r,"P",{});var M=c(d);f=S(M,`Jeder Mensch ist einzigartig und so wie er ist vollkommen. Wir erhalten st\xE4ndig die n\xF6tigen
+        Informationen, um als \u201ESystem Mensch\u201C vital zu bleiben. H\xE4ufig verstehen wir diese
+        Botschaften nicht mehr und geraten aus unserer Mitte. Auf der Suche nach einer L\xF6sung
+        \xFCbersehen wir, dass wir selbst f\xFCr uns verantwortlich sind. In der Folge reagieren wir
+        entweder gar nicht oder unangemessen. Jede Ver\xE4nderung im System tr\xE4gt die Information der
+        L\xF6sung schon in sich.`),M.forEach(s),g=z(r),o=h(r,"P",{});var H=c(o);v=S(H,`Meine Gabe und damit Aufgabe ist es, die in diesem System innewohnenden Botschaften, also
+        L\xF6sungsans\xE4tze zu sehen und in eine f\xFCr den Klienten verst\xE4ndliche Sprache zu \xFCbersetzen.`),H.forEach(s),r.forEach(s),this.h()},h(){document.title="Home"},m(n,p){I(n,i,p),I(n,e,p),t(e,a),t(a,u),t(e,m),t(e,d),t(d,f),t(e,g),t(e,o),t(o,v)},p:k,i(n){_||J(()=>{_=W(e,P,{}),_.start()})},o:k,d(n){n&&s(i),n&&s(e)}}}const F=!0;class G extends x{constructor(i){super(),E(this,i,null,V,A,{})}}export{G as default,F as prerender};
